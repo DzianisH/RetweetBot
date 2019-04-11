ec2fe5fe6896f3b8a80b3314dacc36fa7643b4f2
Dzianis Haurylavets <dzianis.haurylavets-ext@ax-ao.de>
2019-04-11 23:48:43 +0300

    Monkeycode improvement

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const twitter = new Twitter({
 	access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
+const retweetIds = ['1116226971938091008'];
 twitter.get('account/verify_credentials', {
 	include_entities: false,
 	skip_status: true,
@@ -19,49 +20,73 @@ twitter.get('account/verify_credentials', {
 	console.log('Authentication successful. Running bot...');
 
 	const params = {
-		q: '#fitness',
+		q: '#lifting',
 		result_type: 'recent',
 		lang: 'en',
-		count: 50,
+		count: 100,
 	};
 
-	twitter.get('search/tweets', params, (err, res) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
+	const retweeter = (since_id) => {
+		console.log("RETWETTER IS HERE");
+		params.since_id = since_id;
+		twitter.get('search/tweets', params, (err, res) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
 
-		let statuses = res.statuses
-			.filter(status => !status.possibly_sensitive)
-			.filter(status => !status.retweeted)
-			.filter(status => status.entities.urls.length === 0)
-			.filter(status => !status.in_reply_to_status_id && !status.in_reply_to_user_id && !status.in_reply_to_screen_name)
-			.map(status => {
-				if (status.user.followers_count) {
-					status.popularity = (status.retweet_count + status.favorite_count) / status.user.followers_count;
-				} else {
-					status.popularity = 0.;
-				}
-				return status;
-			});
+			console.log(res.statuses.length);
+			if (res.statuses.length === 0) {
+				console.log("Nothing to retweet");
+				setTimeout(retweeter, 66 * 1000);
+				return;
+			}
 
-		statuses.sort((status1, status2) => status2.popularity - status1.popularity);
-		console.log(statuses);
-		if (statuses.length > 0) {
-			const retwitId = statuses[0].id_str;
+			let statuses = res.statuses;
+			statuses = statuses
+				.filter(status => !status.possibly_sensitive)
+				.filter(status => !status.retweeted)
+				.filter(status => status.entities.urls.length === 0)
+				.filter(status => !status.in_reply_to_status_id && !status.in_reply_to_user_id && !status.in_reply_to_screen_name)
+				.filter(status => !status.retweeted_status)
+				.filter(status => retweetIds.indexOf(status.id_str) === -1)
+				.map(status => {
+					status.popularity = (status.retweet_count + status.favorite_count) / (status.user.followers_count + 2);
+					return status;
+				})
+				.filter(status => status.popularity > 0.001);
 
-			twitter.post('statuses/retweet/:id', {
-				id: retwitId
-			}, (err, res) => {
-				if (err) {
-					console.error('Something went wrong while RETWEETING... Duplication maybe...');
-					console.error(err);
-				} else if (res) {
-					console.log('Retweeted!!!');
-					console.log(res);
-				}
-			});
-		}
-	});
+			if (statuses.length > 0) {
+				statuses.sort((status1, status2) => status2.popularity - status1.popularity);
+				const retweetId = statuses[0].id_str;
+				console.log(statuses[0]);
+				console.log("http://twitter.com/" + statuses[0].user.screen_name + "/status/" + retweetId);
+				console.log(statuses[0].popularity);
+
+
+				twitter.post('statuses/retweet/:id', {
+					id: retweetId
+				}, (err, res) => {
+					if (err) {
+						console.error('Something went wrong while RETWEETING... Duplication maybe...');
+						console.error(err);
+						if(retweetIds.indexOf(retweetId) === -1) {
+							retweetIds.push(retweetId);
+						}
+						setTimeout(retweeter,66 * 1000);
+					} else if (res) {
+						retweetIds.push(retweetId);
+						console.log('retwetted');
+						setTimeout(retweeter, 35 * 60 * 1000);
+					}
+				});
+			} else {
+				console.log('Nothing to retweet');
+				setTimeout(retweeter, 66 * 1000);
+			}
+		});
+	};
+
+	retweeter(undefined)
 });
 
