55757f95a5f2699c4c4f73a29b7dcf10e65e9978
Dzianis Haurylavets <dzianis.haurylavets-ext@ax-ao.de>
2019-05-12 00:06:37 +0300

    Create single class of tasks instead of function set

--- tasks.js
+++ tasks.js
@@ -1,19 +1,137 @@
 const Twitter = require('twit');
-const config = require('./config');
 
-const twitter = new Twitter(config.twitter);
-let rtIdList = [];
+export class Tasks {
+	constructor(config) {
+		this.config = config;
+		this.twitter = new Twitter(this.config.twitter);
+		this.rtIdList = [];
+	}
 
-const randomElement = (arr) => {
-	const index = Math.floor(arr.length * Math.random());
-	return arr[index];
-};
+	loginTask(callback) {
+		this.twitter.get('account/verify_credentials', {
+			include_entities: false,
+			skip_status: true,
+			include_email: false
+		}, (err) => {
+			if (err) {
+				this.err("Can't login");
+			}
+			return callback(err);
+		});
+	};
 
-function createQuote(tweet) {
+	fetchTweetsTask(callback) {
+		const hashTag = randomElement(this.config.hashTags);
+		const params = {
+			q: hashTag,
+			result_type: this.config.resultType,
+			lang: this.config.lang,
+			count: this.config.count,
+		};
+
+		this.twitter.get('search/tweets', params, (err, res) => {
+			if (err) {
+				this.err("Can't get latest tweets");
+			}
+			return callback(err, res);
+		});
+	};
+
+	filterTweetsTask(res, callback) {
+		const statuses = res.statuses
+			.filter(status => !!status)
+			.filter(status => !status.possibly_sensitive)
+			.filter(status => !status.retweeted)
+			.filter(status => status.entities.urls.length <= this.config.maxUrlsCount)
+			.filter(status => !status.in_reply_to_status_id && !status.in_reply_to_user_id && !status.in_reply_to_screen_name)
+			.filter(status => !status.retweeted_status)
+			.filter(status => this.rtIdList.indexOf(status.id_str) === -1)
+			.map(status => {
+				status.popularity = (status.retweet_count + status.favorite_count) /
+					(Math.log(status.user.followers_count + 1) + 1);
+				return status;
+			})
+			.filter(status => status.popularity > 0);
+
+		if (statuses.length === 0) {
+			return callback("Nothing to retweet");
+		}
+
+		statuses.sort((status1, status2) => status2.popularity - status1.popularity);
+		return callback(null, statuses[0]);
+	};
+
+	repostTask(tweet, callback) {
+		if (Math.random() < this.config.quoteRate) {
+			this.quoteTask(tweet, callback);
+		} else {
+			this.retweetTask(tweet, callback);
+		}
+	};
+
+	retweetTask(tweet, callback) {
+		this.twitter.post('statuses/retweet/:id', {
+			id: tweet.id_str
+		}, (err, res) => {
+
+			if (err) {
+				this.err("Can't retweet " + createTweetLink(tweet));
+			} else {
+				this.log("Retweeted " + createTweetLink(tweet));
+			}
+
+			this.pushTweetToRTList(tweet);
+			return callback(err, res);
+		});
+	};
+
+	quoteTask(tweet, callback) {
+		this.twitter.post('statuses/update', {
+			status: createQuote(tweet),
+		}, (err, res) => {
+
+			if (err) {
+				this.err("Can't quote " + createTweetLink(tweet));
+			} else {
+				this.log("Quoted " + createTweetLink(tweet));
+			}
+
+			this.pushTweetToRTList(tweet);
+			return callback(err, res);
+		});
+	};
+
+	pushTweetToRTList(tweet) {
+		if (this.rtIdList.indexOf(tweet.id_str) === -1) {
+			this.rtIdList.push(tweet.id_str);
+		}
+		if (this.rtIdList.length > 151) {// magic numbers is ok
+			this.rtIdList = this.rtIdList.slice(this.rtIdList.length - 50, this.rtIdList.length);
+		}
+	}
+
+	// will redo later
+	log(text) {
+		if (text) {
+			console.log(this.config.name + ' ' + text);
+		}
+	}
+
+	err(err, text) {
+		if (text) {
+			console.error(this.config.name + ' ' + text);
+		}
+		if (err) {
+			console.error(err);
+		}
+	}
+}
+
+const createQuote = (tweet) => {
 	return createQuoteMessage(tweet)
 		+ '\n' + createQuoteHashTags(tweet)
 		+ '\n' + createTweetLink(tweet);
-}
+};
 
 const createQuoteHashTags = (status) => {
 	const hashTags = status.entities.hashtags;
@@ -39,115 +157,7 @@ function createTweetLink(tweet) {
 	return "http://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
 }
 
-const loginTask = (callback) => {
-	twitter.get('account/verify_credentials', {
-		include_entities: false,
-		skip_status: true,
-		include_email: false
-	}, (err) => {
-		if (err) {
-			console.error("Can't login");
-		}
-		return callback(err);
-	});
-};
-
-const fetchTweetsTask = (callback) => {
-	const hashTag = randomElement(config.hashTags);
-	const params = {
-		q: hashTag,
-		result_type: config.resultType,
-		lang: config.lang,
-		count: config.count,
-	};
-	twitter.get('search/tweets', params, (err, res) => {
-		if (err) {
-			console.error("Can't get latest tweets");
-		}
-		return callback(err, res);
-	});
-};
-
-const filterTweetsTask = (res, callback) => {
-	const statuses = res.statuses
-		.filter(status => !!status)
-		.filter(status => !status.possibly_sensitive)
-		.filter(status => !status.retweeted)
-		.filter(status => status.entities.urls.length <= config.maxUrlsCount)
-		.filter(status => !status.in_reply_to_status_id && !status.in_reply_to_user_id && !status.in_reply_to_screen_name)
-		.filter(status => !status.retweeted_status)
-		.filter(status => rtIdList.indexOf(status.id_str) === -1)
-		.map(status => {
-			status.popularity = (status.retweet_count + status.favorite_count) /
-				(Math.log(status.user.followers_count + 1) + 1);
-			return status;
-		})
-		.filter(status => status.popularity > 0);
-
-	if (statuses.length === 0) {
-		return callback("Nothing to retweet");
-	}
-
-	statuses.sort((status1, status2) => status2.popularity - status1.popularity);
-	return callback(null, statuses[0]);
-};
-
-const repostTask = (tweet, callback) => {
-	if (Math.random() < config.quoteRate) {
-		quoteTask(tweet, callback);
-	} else {
-		retweetTask(tweet, callback);
-	}
-};
-
-const retweetTask = (tweet, callback) => {
-	twitter.post('statuses/retweet/:id', {
-		id: tweet.id_str
-	}, (err, res) => {
-		const tweetLink = "http://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
-
-		if (err) {
-			console.error("Can't retweet " + tweetLink);
-		} else {
-			console.log("Retweeted " + tweetLink);
-		}
-
-		if(rtIdList.indexOf(tweet.id_str) === -1) {
-			rtIdList.push(tweet.id_str);
-		}
-		if (rtIdList.length > 151) {// magic numbers is ok
-			rtIdList = rtIdList.slice(rtIdList.length - 50, rtIdList.length);
-		}
-		return callback(err, res);
-	});
-};
-
-const quoteTask = (tweet, callback) => {
-	twitter.post('statuses/update', {
-		status: createQuote(tweet),
-	}, (err, res) => {
-
-		if (err) {
-			console.error("Can't quote" + createTweetLink(tweet));
-		} else {
-			console.log("Quoted " + createTweetLink(tweet));
-		}
-
-		if(rtIdList.indexOf(tweet.id_str) === -1) {
-			rtIdList.push(tweet.id_str);
-		}
-		if (rtIdList.length > 151) {// magic numbers is ok
-			rtIdList = rtIdList.slice(rtIdList.length - 50, rtIdList.length);
-		}
-		return callback(err, res);
-	});
-};
-
-module.exports = {
-	loginTask: loginTask,
-	fetchTweetsTask: fetchTweetsTask,
-	filterTweetsTask: filterTweetsTask,
-	repostTask: repostTask,
-	retweetTask: retweetTask,
-	quoteTask: quoteTask
+const randomElement = (arr) => {
+	const index = Math.floor(arr.length * Math.random());
+	return arr[index];
 };
